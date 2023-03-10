34755b29e69f1ac8ffe800ced8bbe97cfd354d4c
anipatil72 <anipatil920@gmail.com>
2023-03-10 13:10:14 +0530

    Second Commit

---
+++ 789.cpp
@@ -0,0 +1,32 @@
+// ॥ श्री गणेशाय नमः ॥
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int findCity(int n, int m, vector<vector<int>> &edges, int distanceThreshold)
+{
+    vector<vector<int>> mat(n, vector<int>(n, (int)1e9));
+
+    for (int i = 0; i < n; i++)
+    {
+        mat[i][i] = 0 ;
+    }
+
+    
+    
+
+
+
+
+
+}
+
+int main()
+{
+
+    return 0;
+}
+
+// ॥ जय श्री राम जय श्री कृष्ण ॥
