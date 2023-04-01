2a3252498a1dffa10cfb137967708d415070e742
Aniruddha <anipatil920@gmail.com>
2023-04-01 23:57:10 +0530

    Today's commit

---
+++ C_Hexadecimal_s_Numbers.cpp
@@ -0,0 +1,122 @@
+// ॥ श्री गणेशाय नमः ॥
+
+#include <bits/stdc++.h>
+#include <random>
+#include <complex>
+#include <tuple>
+#include <functional>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+#include <cstdint>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define uint unsigned int
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRandomNumber(ll B)
+{
+    return (ull)rng() % B;
+}
+int GCD(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+
+    return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+    return (a * b) / GCD(a, b);
+}
+
+// returns map containing factor and multiplicity, Eg: 60 = {{2,2},{3,1},{5,1}}
+
+map<long long, long long> factorize(long long n)
+{
+    map<long long, long long> ans;
+    for (long long i = 2; i * i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            ans[i]++;
+            n /= i;
+        }
+    }
+    if (n > 1)
+    {
+        ans[n]++;
+        n = 1;
+    }
+    return ans;
+}
+
+// -------------------Important Notes------------------- //
+// ***For Interactive Problems remember to remove multi test cases condition*** //
+// (int)log2(x) + 1  -> to calculate number of bits of a number
+// s.erase(0, min(s.find_first_not_of('0'), s.size()-1));   //for removing leading zero's
+// Remember about unique function usage
+// yi=(t|xi)⊕xi
+// (int index = str.find(substr, pos)) != string::npos    // for checking substring 'substr' in string 'str'
+// ((x >> j) & 1) ===> to check if the j-th bit is on/off
+// log2(n&(-n)) returns a number in which only the rightmost bit is set in n
+// alternatively ffs(n) also gives the index of the rightmeost set bit
+// x |= (1 << i) ===> to set the i-th bit on
+
+void solve()
+{
+    int n;
+
+    cin >> n;
+
+    string s = to_string(n);
+
+    int len = s.length();
+
+    int ans = 1;
+
+    for (int i = 0; i < len; i++)
+    {
+        if ((s[i] - '0') >= 1)
+        {
+            ans *= 2;
+        }
+        else
+        {
+
+            ans *= 1;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int n, cnt;
+
+void p(int a)
+{
+    if (a <= n)
+    {
+        cnt++;
+        p(a * 10);
+        p(a * 10 + 1);
+    }
+}
+
+int main()
+{
+    std::cin >> n;
+    p(1);
+    std::cout << cnt;
+}
+
+// ॥ जय श्री राम जय श्री कृष्ण ॥
