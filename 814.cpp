e3131f7f87fcb48f183159ad9cfbb424900f7371
Aniruddha <anipatil920@gmail.com>
2023-04-01 23:15:15 +0530

    After contest commit

---
+++ 814.cpp
@@ -0,0 +1,18 @@
+// ॥ श्री गणेशाय नमः ॥
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+
+    cout << "security" << endl;
+
+    return 0;
+}
+
+// ॥ जय श्री राम जय श्री कृष्ण ॥
