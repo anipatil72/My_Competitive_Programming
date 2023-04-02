acc2dc1ea0622fece16fd5c55b112a609ebe10fe
Aniruddha Patil <r.patil@iitg.ac.in>
2023-04-02 22:17:15 +0530

    After Codeforces Contest 862 Div.2

---
+++ 819.cpp
@@ -0,0 +1,24 @@
+// ॥ श्री गणेशाय नमः ॥
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cin >> n;
+
+    vector<int> dp(n + 1);
+
+    dp[1] = 1;
+
+    return 0;
+}
+
+// ॥ जय श्री राम जय श्री कृष्ण ॥
