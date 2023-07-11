0912c79c44ada464e6b0aa7f52e792e27e9bb7d4
Aniruddha Patil <r.patil@iitg.ac.in>
2023-07-11 23:44:24 +0530

    Finally fixed that bug!

--- 1081.cpp
+++ 1081.cpp
@@ -11,6 +11,42 @@ using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 // typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template <class Fun>
+class y_combinator_result
+{
+    Fun fun_;
+
+public:
+    template <class T>
+    explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+    template <class... Args>
+    decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
+};
+template <class Fun>
+decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << "," << p.second << ')'; }
+template <typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+    os << '{';
+    string sep;
+    for (const T &x : v)
+        os << sep << x, sep = ", ";
+    return os << '}';
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+    cerr << ' ' << H;
+    dbg_out(T...);
+}
+#define dbg(...) cerr << '[' << __FILE__ << ':' << __LINE__ << "] (" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
 const int MAX = 10000;
 
 int findK(vector<vector<int>> &a, int n, int m, int k)
@@ -23,14 +59,19 @@ int findK(vector<vector<int>> &a, int n, int m, int k)
     {
         if (count % 4 == 0)
         {
+            // dbg(left, right, up, down);
 
             for (int i = left; i <= right && des < k; i++)
             {
                 des++;
 
-                ans = a[up][i];
+                // cout << "These are indices : " << up << " " << i << endl;
 
-                cout << ans << endl;
+                // cout << a[up][i] << " ";
+
+                // dbg(up, i);
+
+                ans = a[up][i];
             }
 
             if (des == k)
@@ -40,15 +81,24 @@ int findK(vector<vector<int>> &a, int n, int m, int k)
 
             up++;
         }
-        else if (count % 4 == 1)
+
+        if (count % 4 == 1)
         {
+            // dbg(left, right, up, down);
+
             for (int i = up; i <= down && des < k; i++)
             {
                 des++;
 
+                // cout << "These are indices : " << i << " " << right << endl;
+
+                // dbg(i, right);
+
                 ans = a[i][right];
 
-                cout << ans << endl;
+                // cout << a[i][right] << " ";
+
+                // cout << ans << endl;
             }
 
             if (des == k)
@@ -58,16 +108,24 @@ int findK(vector<vector<int>> &a, int n, int m, int k)
 
             right--;
         }
-        else if (count % 4 == 2)
+
+        if (count % 4 == 2)
         {
+            // dbg(left, right, up, down);
 
             for (int i = right; i >= left && des < k; i--)
             {
                 des++;
 
+                // cout << "These are indices : " << down << " " << i << endl;
+
+                // dbg(down, i);
+
                 ans = a[down][i];
 
-                cout << ans << endl;
+                // cout << a[down][i] << " ";
+
+                // cout << ans << endl;
             }
 
             if (des == k)
@@ -77,16 +135,24 @@ int findK(vector<vector<int>> &a, int n, int m, int k)
 
             down--;
         }
-        else
+
+        if (count % 4 == 3)
         {
+            // dbg(left, right, up, down);
 
             for (int i = down; i >= up && des < k; i--)
             {
                 des++;
 
+                // cout << "These are indices : " << i << " " << left << endl;
+
+                // dbg(i, left);
+
                 ans = a[i][left];
 
-                cout << ans << endl;
+                // cout << a[i][left] << " ";
+
+                // cout << ans << endl;
             }
 
             if (des == k)
@@ -97,7 +163,7 @@ int findK(vector<vector<int>> &a, int n, int m, int k)
             left++;
         }
 
-        count++;
+        // count++;
     }
 
     return ans;
@@ -106,11 +172,13 @@ int findK(vector<vector<int>> &a, int n, int m, int k)
 int main()
 {
 
-    vector<vector<int>> spiral = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> spiral = {{1, 2, 3},
+                                  {4, 5, 6},
+                                  {7, 8, 9}};
 
-    int k = 4;
+    int k = 9;
 
-    cout << "This is ans : " << findK(spiral, 3, 3, k) << endl;
+    cout << findK(spiral, 3, 3, k) << endl;
 
     return 0;
 }
