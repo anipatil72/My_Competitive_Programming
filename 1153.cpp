7ed6856addfc6dc11702bc33a989eb65d57d076e
Aniruddha Patil <r.patil@iitg.ac.in>
2023-09-30 10:36:03 +0530

    Added some informatory comments!

--- 1153.cpp
+++ 1153.cpp
@@ -11,6 +11,8 @@ using namespace __gnu_pbds;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 // typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
+// This is all very standard nothing out of the ordianry
+
 void booleanMatrix(vector<vector<int>> &matrix)
 {
     int n = matrix.size();
